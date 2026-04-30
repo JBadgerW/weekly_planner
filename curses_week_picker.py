f076ba717b0fb5328a5e7ae9155c16a0f477a046
Jon Watson <jonbwatson@gmail.com>
2026-04-30 06:26:44 -0400

    Added a left spacer so the calendar isn't smack up against the left border of the terminal.

--- curses_week_picker.py
+++ curses_week_picker.py
@@ -2,6 +2,9 @@ import curses
 import calendar
 import datetime
 
+LEFT_SPACER = 3
+TOP_SPACER = 3
+
 
 def date_picker(stdscr):
     calendar.setfirstweekday(calendar.SUNDAY)
@@ -9,16 +12,16 @@ def date_picker(stdscr):
     curses.start_color()
     curses.use_default_colors()
 
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_WHITE, -1)
     stdscr.bkgd(" ", curses.color_pair(1))
 
     if curses.COLORS >= 256:
-        curses.init_pair(2, 8, curses.COLOR_BLACK)
+        curses.init_pair(2, 8, -1)
         curses.init_pair(3, 8, curses.COLOR_WHITE)
         gray_unselected = curses.color_pair(2)
         gray_selected = curses.color_pair(3)
     else:
-        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_WHITE, -1)
         curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
         gray_unselected = curses.color_pair(2) | curses.A_DIM
         gray_selected = curses.color_pair(3) | curses.A_DIM
@@ -38,8 +41,8 @@ def date_picker(stdscr):
         selected_week = min(selected_week, len(cal) - 1)
 
         header = f"{calendar.month_name[month]} {year}"
-        stdscr.addstr(0, 0, header.center(20), curses.A_BOLD)
-        stdscr.addstr(1, 0, "Su Mo Tu We Th Fr Sa")
+        stdscr.addstr(0, LEFT_SPACER, header.center(20), curses.A_BOLD)
+        stdscr.addstr(1, LEFT_SPACER, "Su Mo Tu We Th Fr Sa")
 
         for i, week in enumerate(cal):
             is_selected_week = i == selected_week
@@ -47,7 +50,10 @@ def date_picker(stdscr):
             if is_selected_week:
                 # Draws the highlight bar across the full width
                 stdscr.addstr(
-                    2 + i, 0, " " * 20, curses.color_pair(1) | curses.A_REVERSE
+                    2 + i,
+                    LEFT_SPACER,
+                    " " * 20,
+                    curses.color_pair(1) | curses.A_REVERSE,
                 )
 
             for j, day_date in enumerate(week):
@@ -62,7 +68,7 @@ def date_picker(stdscr):
                 else:
                     attr = gray_selected if is_selected_week else gray_unselected
 
-                stdscr.addstr(2 + i, j * 3, f"{day_date.day:2}", attr)
+                stdscr.addstr(2 + i, j * 3 + LEFT_SPACER, f"{day_date.day:2}", attr)
 
         stdscr.refresh()
         key = stdscr.getch()
